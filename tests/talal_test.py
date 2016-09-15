59810781d2b5c886b91fdaa3f9e14c8613154e4b
Anahit Sarao <indianvip60@gmail.com>
2016-09-15 16:11:25 -0700

    fixed custom testing

---
+++ tests/talal_test.py
@@ -0,0 +1,22 @@
+from talalprotocol.talalprotocol import *
+
+'''
+This is just a test file used for
+1. testing functionality
+2. debugging
+'''
+
+x = caesar_cipher()
+print(x)
+
+y = caesar_decipher()
+print(y)
+
+z = vigenere_cipher()
+print(z)
+
+a = vigenere_decipher()
+print(a)
+
+
+
