5472144a666145e36be12d551cb80716bb360169
Talal Khalil <talal2007khalil@yahoo.com>
2016-09-15 13:02:20 -0700

    modified test file

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 from talalprotocol import *
 
+'''
+This is just a test file used for 
+1. testing functionality 
+2. debugging 
+'''
 
 x = caesar_cipher() 
+print(x)
+
+y = caesar_decipher()
+print(y)
+
+z = vigenere_cipher()
+print(z)
+
+a = vigenere_decipher()
+print(a)
