86c51985861f1ea635b52a7c33ec78c44a39568e
Talal Khalil <talal2007khalil@yahoo.com>
2016-09-17 14:39:13 -0700

    Added various test cases. More to be added.

--- tests/talal_test.py
+++ tests/talal_test.py
@@ -6,12 +6,45 @@ This is just a test file used for
 2. debugging
 '''
 
+#<---------- Caesar testing cases ---------->#
 
+#test case 1
 original_message = 'Nadim is .... a faggot bitch ####!'
-shift_val = 1
+shift_val = -2
 x = str(caesar_cipher(original_message,shift_val))
 print('The original message is: ' + original_message)
 print('The encrypted text is: ' + x)
 
-y = caesar_decipher(x, 1)
-print('The decrypted message is: ' + y)
+
+#test case 2
+a = caesar_decipher(message='I am gay', shift_value=1)
+print(a)
+
+
+#test case 3
+my_shift = 2
+b = caesar_cipher(message='I am a pansy', my_shift)
+print(b)
+
+#test case 4
+my_message = 'Imma rob a candy shop REAL QUICKIE !!!!###'
+c = caesar_cipher(my_message, shift_value= 4)
+
+
+#test case 5
+d = caesar_cipher()
+print(d)
+
+
+#test case 6
+e = caesar_cipher('hi')
+print(e)
+
+# test case 7
+f = caesar_cipher(1, 'hi')
+print(f)
+
+
+
+
+
