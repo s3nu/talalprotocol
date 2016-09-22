f1d548188c2f9062e53635c90954880c386776be
Talal Khalil <talal2007khalil@yahoo.com>
2016-09-22 13:16:48 -0700

    Added binary decryption algorithm

--- talalprotocol/talalprotocol.py
+++ talalprotocol/talalprotocol.py
@@ -211,10 +211,8 @@ def vigenere_decipher(message, key):
     for character in message:
         if character.isdigit():
             decrypted_message = decrypted_message + character
-
         elif character in string_punctuation:
             decrypted_message = decrypted_message + character
-
         elif character in english_alphabets:
             new_index = english_alphabets.index(character) - \
                         english_alphabets.index(key_list[key_pointer])
@@ -228,21 +226,22 @@ def vigenere_decipher(message, key):
     return decrypted_message
 
 
-
 def converting_to_binary(item1):
 
     digit_list = list(string.digits)
     string_punctuation = list(string.punctuation)
 
     if item1 in digit_list:
-        # added 32 = 100000 as a flag bit to indicate that a char is a digit
+        # added 32 = 0100000 as a flag bit to indicate that a char is a digit
         item1 = int(item1) + 32
-        binary_value = bin(item1)[2:].zfill(6)
+        binary_value = bin(item1)[2:].zfill(7)
+    elif item1 in string_punctuation:
+        item1 = string_punctuation.index(item1) + 96
+        binary_value = bin(item1)[2:].zfill(7)
     else:
-        binary_value = bin(item1)[2:].zfill(6)
+        binary_value = bin(item1)[2:].zfill(7)
 
     return binary_value
-
 
 
 def binary_encryption(message):
@@ -263,10 +262,9 @@ def binary_encryption(message):
         if char.isdigit():
             digit_in_binary = converting_to_binary(char)
             binary_ciphered_message += digit_in_binary
-
         elif char in string_punctuation:
-            binary_ciphered_message += char
-
+            punc_in_binary = converting_to_binary(char)
+            binary_ciphered_message += punc_in_binary
         elif char in english_alphabets:
             index = english_alphabets.index(char)
             x = converting_to_binary(index)
@@ -276,6 +274,29 @@ def binary_encryption(message):
 
     return binary_ciphered_message
 
+
+
+def binary_decryption(encrypted_message):
+
+    chunks = encrypted_message.split(' ')
+    decrypted_message = ''
+
+    for i in chunks:
+        flag = i[:2]
+        if flag == '00':
+            index = int(i[2:], 2)
+            decrypted_message += english_alphabets[index]
+        elif flag == '01':
+             digit = int(i[2:], 2)
+             digit = str(digit)
+             decrypted_message += digit
+        elif flag == '10':
+            punctuation_index = int(i[2:], 2)
+            decrypted_message += string_punctuation[punctuation_index]
+        else:
+            print('Error!')
+
+    return decrypted_message
 
 
 def main():
