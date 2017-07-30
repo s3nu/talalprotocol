f997680602a991e59d322bfb9579f488f24f9e1b
Anahit Sarao <indianvip60@gmail.com>
2017-07-29 23:32:01 -0700

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='talalprotocol',
-    version='0.2.1',
+    version='0.3.0',
     description="Deep Layered Encryption",
     long_description=readme + '\n\n' + history,
     author="Team Standy",
